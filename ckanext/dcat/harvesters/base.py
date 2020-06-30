e1193e2cc8544ee6c37c66bec263ea0d375a189b
dvasilov <dvasilov@opengov.com>
2020-06-30 18:45:30 +0300

    DEbug commit

--- ckanext/dcat/harvesters/base.py
+++ ckanext/dcat/harvesters/base.py
@@ -1,24 +1,21 @@
-import os
-import uuid
 import logging
+import os
 import re
 
-import requests
 import rdflib
-
-from ckan import plugins as p
-from ckan import logic
+import requests
 from ckan import model
-
-from ckan.logic import ValidationError, NotFound, get_action
+from ckan import plugins as p
 from ckan.lib.helpers import json
 from ckan.lib.munge import substitute_ascii_equivalents
-
-from ckanext.harvest.harvesters import HarvesterBase
-from ckanext.harvest.model import HarvestObject, HarvestObjectExtra
-
+from ckanext.dcat.harvesters.configuration_processors import (
+    ResourceOrder, ContactPoint,
+    DefaultTags, Publisher, CleanTags,
+    DefaultExtras, DefaultGroups, DefaultValues,
+    MappingFields, OrganizationFilter)
 from ckanext.dcat.interfaces import IDCATRDFHarvester
-
+from ckanext.harvest.harvesters import HarvesterBase
+from ckanext.harvest.model import HarvestObject
 
 log = logging.getLogger(__name__)
 
@@ -31,6 +28,7 @@ def _munge_to_length(string, min_length, max_length):
         string = string[:max_length]
     return string
 
+
 def munge_tag(tag):
     tag = substitute_ascii_equivalents(tag)
     tag = tag.lower().strip()
@@ -40,13 +38,24 @@ def munge_tag(tag):
 
 
 class DCATHarvester(HarvesterBase):
-
     MAX_FILE_SIZE = 1024 * 1024 * 50  # 50 Mb
     CHUNK_SIZE = 1024
 
     force_import = False
 
     config = None
+    processers = [
+        DefaultTags,
+        CleanTags,
+        DefaultGroups,
+        DefaultExtras,
+        DefaultValues,
+        MappingFields,
+        Publisher,
+        ContactPoint,
+        ResourceOrder,
+        OrganizationFilter
+    ]
 
     def _get_content_and_type(self, url, harvest_job, page=1,
                               content_type=None):
@@ -124,8 +133,8 @@ class DCATHarvester(HarvesterBase):
                 # We want to catch these ones later on
                 raise
 
-            msg = 'Could not get content from %s. Server responded with %s %s'\
-                % (url, error.response.status_code, error.response.reason)
+            msg = 'Could not get content from %s. Server responded with %s %s' \
+                  % (url, error.response.status_code, error.response.reason)
             self._save_gather_error(msg, harvest_job)
             return None, None
         except requests.exceptions.ConnectionError, error:
@@ -134,8 +143,8 @@ class DCATHarvester(HarvesterBase):
             self._save_gather_error(msg, harvest_job)
             return None, None
         except requests.exceptions.Timeout, error:
-            msg = 'Could not get content from %s because the connection timed'\
-                ' out.' % url
+            msg = 'Could not get content from %s because the connection timed' \
+                  ' out.' % url
             self._save_gather_error(msg, harvest_job)
             return None, None
 
@@ -165,7 +174,7 @@ class DCATHarvester(HarvesterBase):
 
     def get_original_url(self, harvest_object_id):
         obj = model.Session.query(HarvestObject). \
-            filter(HarvestObject.id == harvest_object_id).\
+            filter(HarvestObject.id == harvest_object_id). \
             first()
         if obj:
             return obj.source.url
@@ -179,11 +188,11 @@ class DCATHarvester(HarvesterBase):
         '''
 
         datasets = model.Session.query(model.Package.id) \
-                                .join(model.PackageExtra) \
-                                .filter(model.PackageExtra.key == 'guid') \
-                                .filter(model.PackageExtra.value == guid) \
-                                .filter(model.Package.state == 'active') \
-                                .all()
+            .join(model.PackageExtra) \
+            .filter(model.PackageExtra.key == 'guid') \
+            .filter(model.PackageExtra.value == guid) \
+            .filter(model.Package.state == 'active') \
+            .all()
 
         if not datasets:
             return None
@@ -201,115 +210,11 @@ class DCATHarvester(HarvesterBase):
             creating or updating the actual package.
         '''
 
-        self._set_config(harvest_object.job.source.config)
+        config_str = harvest_object.job.source.config
+        self.config = json.loads(config_str) if config_str else {}
 
-        # Set default tags if needed
-        default_tags = self.config.get('default_tags', [])
-        if default_tags:
-            if not 'tags' in package_dict:
-                package_dict['tags'] = []
-            package_dict['tags'].extend(
-                [t for t in default_tags if t not in package_dict['tags']])
-
-        # clean tags of invalid characters
-        tags = package_dict.get('tags', [])
-        package_dict['tags'] = self._clean_tags(tags)
-
-        # Set default groups if needed
-        default_groups = self.config.get('default_groups', [])
-        if default_groups:
-            if not 'groups' in package_dict:
-                package_dict['groups'] = []
-            existing_group_ids = [g['id'] for g in package_dict['groups']]
-            package_dict['groups'].extend(
-                [{'name':g['name']} for g in self.config['default_group_dicts']
-                 if g['id'] not in existing_group_ids])
-
-        # Set default extras if needed
-        default_extras = self.config.get('default_extras', {})
-        def get_extra(key, package_dict):
-            for extra in package_dict.get('extras', []):
-                if extra['key'] == key:
-                    return extra
-
-        if not 'extras' in package_dict:
-            package_dict['extras'] = []
-
-        if default_extras:
-            override_extras = self.config.get('override_extras', False)
-            for key, value in default_extras.iteritems():
-                existing_extra = get_extra(key, package_dict)
-                if existing_extra and not override_extras:
-                    continue  # no need for the default
-                if existing_extra:
-                    package_dict['extras'].remove(existing_extra)
-
-                package_dict['extras'].append({'key': key, 'value': value})
-
-        # set default values from config
-        default_values = self.config.get('default_values', [])
-        if default_values:
-            for default_field in default_values:
-                for key in default_field:
-                    package_dict[key] = default_field[key]
-                    # Remove from extras any keys present in the config
-                    existing_extra = get_extra(key, package_dict)
-                    if existing_extra:
-                        package_dict['extras'].remove(existing_extra)
-
-        # set the mapping fields its corresponding default_values
-        map_fields = self.config.get('map_fields', [])
-        if map_fields:
-            for map_field in map_fields:
-                source_field = map_field.get('source')
-                target_field = map_field.get('target')
-                default_value = map_field.get('default')
-                value = dcat_dict.get(source_field, default_value)
-                # If value is a list, convert to string
-                if isinstance(value, list):
-                    value = ', '.join(str(x) for x in value)
-                package_dict[target_field] = value
-                # Remove from extras any keys present in the config
-                existing_extra = get_extra(target_field, package_dict)
-                if existing_extra:
-                    package_dict['extras'].remove(existing_extra)
-
-        # set the publisher
-        publisher_mapping = self.config.get('publisher', {})
-        publisher_field = publisher_mapping.get('publisher_field')
-        if publisher_field:
-            publisher = dcat_dict.get('publisher', {})
-            publisher_name = publisher.get('name') or \
-                             publisher_mapping.get('default_publisher')
-            package_dict[publisher_field] = publisher_name
-            # Remove from extras any keys present in the config
-            existing_extra = get_extra(publisher_field, package_dict)
-            if existing_extra:
-                package_dict['extras'].remove(existing_extra)
-
-        # set the contact point
-        contact_point_mapping = self.config.get('contact_point', {})
-        name_field = contact_point_mapping.get('name_field')
-        email_field = contact_point_mapping.get('email_field')
-        contactPoint = dcat_dict.get('contactPoint',{})
-
-        if name_field:
-            contactPointName = contactPoint.get('fn') or \
-                               contact_point_mapping.get('default_name')
-            package_dict[name_field] = contactPointName
-            # Remove from extras the name field
-            existing_extra = get_extra(name_field, package_dict)
-            if existing_extra:
-                package_dict['extras'].remove(existing_extra)
-
-        if email_field:
-            contactPointEmail = contactPoint.get('hasEmail', ':').split(':')[-1] or \
-                                contact_point_mapping.get('default_email')
-            package_dict[email_field] = contactPointEmail
-            # Remove from extras the email field
-            existing_extra = get_extra(email_field, package_dict)
-            if existing_extra:
-                package_dict['extras'].remove(existing_extra)
+        for processor in self.processers:
+            processor.modify_package_dict(package_dict, self.config, dcat_dict)
 
         return package_dict
 
@@ -324,67 +229,13 @@ class DCATHarvester(HarvesterBase):
         if not config:
             return config
 
-        try:
-            config_obj = json.loads(config)
+        config_obj = json.loads(config)
 
-            if 'default_tags' in config_obj:
-                if not isinstance(config_obj['default_tags'], list):
-                    raise ValueError('default_tags must be a list')
-                if config_obj['default_tags'] and \
-                        not isinstance(config_obj['default_tags'][0], dict):
-                    raise ValueError('default_tags must be a list of dictionaries')
+        # Workers validators could change config object. At least DefaultGroups
+        for worker in self.workers:
+            worker.check_config(config_obj)
 
-            if 'default_groups' in config_obj:
-                if not isinstance(config_obj['default_groups'], list):
-                    raise ValueError('default_groups must be a *list* of group names/ids')
-                if config_obj['default_groups'] and not isinstance(config_obj['default_groups'][0], basestring):
-                    raise ValueError('default_groups must be a list of group names/ids (i.e. strings)')
-
-                # Check if default groups exist
-                context = {'model': model, 'user': p.toolkit.c.user}
-                config_obj['default_group_dicts'] = []
-                for group_name_or_id in config_obj['default_groups']:
-                    try:
-                        group = get_action('group_show')(context, {'id': group_name_or_id})
-                        # save the dict to the config object, as we'll need it
-                        # in the import_stage of every dataset
-                        config_obj['default_group_dicts'].append(group)
-                    except NotFound, e:
-                        raise ValueError('Default group not found')
-                config = json.dumps(config_obj)
-
-            if 'default_extras' in config_obj:
-                if not isinstance(config_obj['default_extras'], dict):
-                    raise ValueError('default_extras must be a dictionary')
-
-            if 'default_values' in config_obj:
-                if not isinstance(config_obj['default_values'], list):
-                    raise ValueError('default_values must be a *list* of dictionaries')
-                if config_obj['default_values'] and not isinstance(config_obj['default_values'][0], dict):
-                    raise ValueError('default_values must be a *list* of dictionaries')
-
-            if 'map_fields' in config_obj:
-                if not isinstance(config_obj['map_fields'], list):
-                    raise ValueError('map_fields must be a *list* of dictionaries')
-                if config_obj['map_fields'] and not isinstance(config_obj['map_fields'][0], dict):
-                    raise ValueError('map_fields must be a *list* of dictionaries')
-
-            if 'publisher' in config_obj:
-                if not isinstance(config_obj['publisher'], dict):
-                    raise ValueError('publisher must be a dictionary')
-
-            if 'contact_point' in config_obj:
-                if not isinstance(config_obj['contact_point'], dict):
-                    raise ValueError('contact_point must be a dictionary')
-
-            if 'organizations_filter_include' in config_obj \
-                and 'organizations_filter_exclude' in config_obj:
-                raise ValueError('Harvest configuration cannot contain both '
-                    'organizations_filter_include and organizations_filter_exclude')
-
-        except ValueError, e:
-            raise e
-
+        config = json.dumps(config_obj, indent=4)
         return config
 
     def _clean_tags(self, tags):
@@ -397,11 +248,11 @@ class DCATHarvester(HarvesterBase):
             # assume it's in the package_show form                    
             tags = [_update_tag(t, 'name', munge_tag(t['name'])) for t in tags if munge_tag(t['name']) != '']
 
-        except TypeError: # a TypeError is raised if `t` above is a string
-           # REST format: 'tags' is a list of strings
-           tags = [munge_tag(t) for t in tags if munge_tag(t) != '']
-           tags = list(set(tags))
-           return tags
+        except TypeError:  # a TypeError is raised if `t` above is a string
+            # REST format: 'tags' is a list of strings
+            tags = [munge_tag(t) for t in tags if munge_tag(t) != '']
+            tags = list(set(tags))
+            return tags
 
         return tags
 
