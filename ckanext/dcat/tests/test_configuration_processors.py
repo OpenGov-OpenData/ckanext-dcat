2794e467af5bb147c6a2e9d095b75a6446271792
Jay Guo <jguo144@gmail.com>
2020-11-17 19:26:33 -0500

    Add tests

---
+++ ckanext/dcat/tests/test_configuration_processors.py
@@ -0,0 +1,446 @@
+import json
+import os
+
+from ckanext.dcat.harvesters.configuration_processors import (
+    DefaultTags, CleanTags,
+    DefaultGroups, DefaultExtras, DefaultValues,
+    MappingFields, Publisher, ContactPoint,
+    OrganizationFilter,
+    ResourceFormatOrder,
+    KeepExistingResources)
+from nose.tools import assert_raises, assert_equal, assert_dict_equal, assert_list_equal
+
+fixtures_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures')
+
+
+def get_package_dict():
+    with open(os.path.join(fixtures_path, 'package_dict.json'), 'r') as f:
+        result = json.load(f)
+    return result
+
+
+def get_dcat_dict():
+    with open(os.path.join(fixtures_path, 'package_dict.json'), 'r') as f:
+        result = json.load(f)
+    return result
+
+
+class TestDefaultTags:
+    @classmethod
+    def setup_class(cls):
+        cls.processor = DefaultTags
+
+    def test_validation_correct_format(self):
+        list_of_dicts_config = {
+            "default_tags": [{"name": "geo"}, {"name": "namibia"}]
+        }
+        self.processor.check_config(list_of_dicts_config)
+
+    def test_validation_wrong_format(self):
+        empty_config = {
+            "default_tags": ""
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(empty_config)
+
+        string_config = {
+            "default_tags": "geo"
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(string_config)
+
+        list_config = {
+            "default_tags": ["geo", "namibia"]
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(list_config)
+
+    def test_modify_package_tags(self):
+        package = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "tags": [{"name": "russian"}, {"name": "tolstoy"}]
+        }
+        config = {
+            "default_tags": [{"name": "geo"}, {"name": "namibia"}]
+        }
+        dcat_dict = {}
+
+        self.processor.modify_package_dict(package, config, dcat_dict)
+
+        tag_names = sorted([tag_dict["name"] for tag_dict in package["tags"]])
+        assert_equal(tag_names, ["geo", "namibia", "russian", "tolstoy"])
+
+
+class TestCleanTags:
+    @classmethod
+    def setup_class(cls):
+        cls.processor = CleanTags
+
+    def test_modify_package_clean_tags(self):
+        package = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "tags": [{"name": "tolstoy!"}]
+        }
+        config = {}
+        dcat_dict = {}
+
+        self.processor.modify_package_dict(package, config, dcat_dict)
+
+        tag_names = sorted([tag_dict["name"] for tag_dict in package["tags"]])
+        assert_equal(tag_names, ["tolstoy"])
+
+
+class TestDefaultGroups:
+    @classmethod
+    def setup_class(cls):
+        cls.processor = DefaultGroups
+
+    def test_modify_package_groups(self):
+        package = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "groups": []
+        }
+        config = {
+            "default_groups": ["science", "spend-data"],
+            "default_group_dicts": [
+                {
+                    "id": "b1084f72-292d-11eb-adc1-0242ac120002",
+                    "name": "science",
+                    "title": "Science",
+                    "display_name": "Science",
+                    "is_organization": False,
+                    "type": "group",
+                    "state": "active"
+                },
+                {
+                    "id": "0d7090cc-12c1-4d19-85ba-9bcfc563ab7e",
+                    "name": "spend-data",
+                    "title": "Spend Data",
+                    "display_name": "Spend Data",
+                    "is_organization": False,
+                    "type": "group",
+                    "state": "active"
+                }
+            ]
+        }
+        dcat_dict = {}
+
+        self.processor.modify_package_dict(package, config, dcat_dict)
+
+        group_names = sorted([group_dict["name"] for group_dict in package["groups"]])
+        assert_equal(group_names, ["science", "spend-data"])
+
+
+class TestDefaultExtras:
+    @classmethod
+    def setup_class(cls):
+        cls.processor = DefaultExtras
+
+    def test_validation_correct_format(self):
+        dict_config = {
+            "default_extras": {
+                "encoding": "utf8"
+            }
+        }
+        self.processor.check_config(dict_config)
+
+    def test_validation_wrong_format(self):
+        empty_config = {
+            "default_extras": ""
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(empty_config)
+
+        list_of_dicts_config = {
+            "default_extras": [{"encoding": "utf8"}]
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(list_of_dicts_config)
+
+    def test_modify_package_extras(self):
+        package = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "extras": []
+        }
+        config = {
+            "default_extras": { "encoding": "utf8" }
+        }
+        dcat_dict = {}
+
+        self.processor.modify_package_dict(package, config, dcat_dict)
+
+        assert_equal(package["extras"][0]["key"], "encoding")
+        assert_equal(package["extras"][0]["value"], "utf8")
+
+
+class TestDefaultValues:
+    @classmethod
+    def setup_class(cls):
+        cls.processor = DefaultValues
+
+    def test_validation_correct_format(self):
+        list_of_dicts_config = {
+            "default_values": [
+                { "notes": "Some notes" }
+            ]
+        }
+        self.processor.check_config(list_of_dicts_config)
+
+    def test_validation_wrong_format(self):
+        empty_config = {
+            "default_values": ""
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(empty_config)
+
+        dict_config = {
+            "default_values": { "notes": "Some notes" }
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(dict_config)
+
+    def test_modify_package_values(self):
+        package = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "notes": ""
+        }
+        config = {
+            "default_values": [
+                { "notes": "Some notes" },
+                { "language": "English" }
+            ]
+        }
+        dcat_dict = {}
+
+        self.processor.modify_package_dict(package, config, dcat_dict)
+
+        assert_equal(package["notes"], "Some notes")
+        assert_equal(package["language"], "English")
+
+
+class TestMappingFields:
+    @classmethod
+    def setup_class(cls):
+        cls.processor = MappingFields
+
+    def test_validation_correct_format(self):
+        list_of_dicts_config = {
+            "map_fields": [
+                {
+                    "source": "language",
+                    "target": "language",
+                    "default": "English"
+                }
+            ]
+        }
+        self.processor.check_config(list_of_dicts_config)
+
+    def test_validation_wrong_format(self):
+        empty_config = {
+            "map_fields": ""
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(empty_config)
+
+        dict_config = {
+            "map_fields": {
+                "source": "language",
+                "target": "language",
+                "default": "English"
+            }
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(dict_config)
+
+    def test_modify_package_mapping_values(self):
+        package = {
+            "title": "Test Dataset",
+            "name": "test-dataset"
+        }
+        config = {
+            "map_fields": [
+                {
+                    "source": "language",
+                    "target": "language",
+                    "default": "English"
+                }
+            ]
+        }
+        dcat_dict = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "language": "Spanish"
+        }
+
+        self.processor.modify_package_dict(package, config, dcat_dict)
+
+        assert_equal(package["language"], "Spanish")
+
+
+class TestPublisher:
+    @classmethod
+    def setup_class(cls):
+        cls.processor = Publisher
+
+    def test_validation_correct_format(self):
+        dict_config = {
+            "publisher": {
+                "publisher_field": "publisher",
+                "default_publisher": "Open Data"
+            }
+        }
+        self.processor.check_config(dict_config)
+
+    def test_validation_wrong_format(self):
+        empty_config = {
+            "publisher": ""
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(empty_config)
+
+        list_of_dicts_config = {
+            "publisher": [
+                { "publisher_field": "publisher" },
+                { "default_publisher": "Open Data" }
+            ]
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(list_of_dicts_config)
+
+    def test_modify_package_publisher_field(self):
+        package = {
+            "title": "Test Dataset",
+            "name": "test-dataset"
+        }
+        config = {
+            "publisher": {
+                "publisher_field": "publisher",
+                "default_publisher": "Open Data"
+            }
+        }
+        dcat_dict = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "publisher": {
+                "name": "U.S. Census"
+            }
+        }
+
+        self.processor.modify_package_dict(package, config, dcat_dict)
+
+        assert_equal(package["publisher"], "U.S. Census")
+
+
+class TestContactPoint:
+    @classmethod
+    def setup_class(cls):
+        cls.processor = ContactPoint
+
+    def test_validation_correct_format(self):
+        dict_config = {
+            "contact_point": {
+                "name_field": "contact_name",
+                "default_name": "nonameprovided",
+                "email_field": "contact_email",
+                "default_email": "noemailprovided@agency.gov"
+            }
+        }
+        self.processor.check_config(dict_config)
+
+    def test_validation_wrong_format(self):
+        empty_config = {
+            "contact_point": ""
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(empty_config)
+
+        list_of_dicts_config = {
+            "contact_point": [
+                { "name_field": "contact_name" },
+                { "default_name": "nonameprovided" },
+                { "email_field": "contact_email" },
+                { "default_email": "noemailprovided@agency.gov" }
+            ]
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(list_of_dicts_config)
+
+    def test_modify_package_contact_fields(self):
+        package = {
+            "title": "Test Dataset",
+            "name": "test-dataset"
+        }
+        config = {
+            "contact_point": {
+                "name_field": "contact_name",
+                "default_name": "None",
+                "email_field": "contact_email",
+                "default_email": "None"
+            }
+        }
+        dcat_dict = {
+            "title": "Test Dataset",
+            "name": "test-dataset",
+            "contactPoint": {
+                "@type": "vcard:Contact",
+                "fn": "Jane Doe",
+                "hasEmail": "mailto:jane.doe@agency.gov"
+            }
+        }
+
+        self.processor.modify_package_dict(package, config, dcat_dict)
+
+        assert_equal(package["contact_name"], "Jane Doe")
+        assert_equal(package["contact_email"], "jane.doe@agency.gov")
+
+
+class TestResourceFormatOrder:
+    @classmethod
+    def setup_class(cls):
+        cls.processor = ResourceFormatOrder
+
+    def test_validation_correct_format(self):
+        config = {
+            "resource_format_order": ["CSV", "ZIP"]
+        }
+        self.processor.check_config(config)
+
+    def test_validation_wrong_format(self):
+        config = {
+            "resource_format_order": "csv zip"
+        }
+        with assert_raises(ValueError):
+            self.processor.check_config(config)
+
+    def test_modify_package_resource_format_order(self):
+        package = get_package_dict()
+        dcat_dict = {}
+        config = {
+            "resource_format_order": ["CSV", "ZIP"]
+        }
+        resources_before = package['resources'][:]
+        self.processor.modify_package_dict(package, config, dcat_dict)
+        resources_after = package['resources'][:]
+
+        resource_order = [f.strip().lower() for f in config["resource_format_order"]]
+        step = 0
+        # Check that reordering resources
+        for i, res_format in enumerate(resource_order):
+            for res in resources_before:
+                step_res_format = res.get('format', '').strip().lower()
+                if res_format == step_res_format:
+                    assert_equal(resources_after[step], res)
+                    step += 1
+
+        # Check all other types of resources
+        for res in resources_before:
+            step_res_format = res.get('format', '').strip().lower()
+            if step_res_format not in resource_order:
+                assert_equal(resources_after[step], res)
+                step += 1
+        # assert_equal(resources_after[0], resources_before[0])
